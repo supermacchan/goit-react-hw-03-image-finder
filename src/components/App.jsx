c6201d3760966e032f8cac0632c530e6125ea7f2
Olga Birkovska <birkovskaya92@ukr.net>
2022-12-07 02:11:59 +0200

    input handle

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,22 @@
 import { Component } from "react";
 import { Searchbar } from "./Searchbar/Searchbar";
+import { ImageGallery } from "./Searchbar/ImageGallery/ImageGallery";
+
 
 export class App extends Component {
-  
+  state = {
+    value: '',
+  }
+
+  formSubmitHandler = ({ value }) => {
+    this.setState({ value });
+  };
+
   render() {
     return (
       <>
-      <Searchbar />
+        <Searchbar onSubmit={this.formSubmitHandler} />
+        <ImageGallery />
       </>
     );
   };
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,26 +1,60 @@
 import PropTypes from 'prop-types';
+import { Component } from 'react';
 import css from './Searchbar.module.css';
 
-export const Searchbar = ({ onSubmit }) => {
-    return (
-        <header className={css.searchbar}>
-            <form className={css.searchForm}>
-                <button type="submit" className={css.searchFormButton}>
-                    <span className={css.searchFormButtonLabel}>Search</span>
-                </button>
+export class Searchbar extends Component {
+    static propTypes = {
+        onSubmit: PropTypes.func.isRequired,
+    };
 
-                <input
-                    className={css.searchFormInput}
-                    type="text"
-                    autoComplete="off"
-                    autoFocus
-                    placeholder="Search images and photos"
-                />
-            </form>
-        </header>
-    );
-};
+    state = {
+        value: '',
+    }
 
-Searchbar.propTypes = {
-    onSubmit: PropTypes.func,
+    hadleInputChange = event => {
+        this.setState({
+            value: event.currentTarget.value,
+        })
+    }
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        if (this.state.value.trim() === '') {
+            this.reset();
+            return;
+        }
+        this.props.onSubmit(this.state);
+        this.reset();
+    }
+
+    reset = () => {
+        this.setState({
+            value: '',
+        });
+    }
+
+    render() {
+        return (
+            <header className={css.searchbar}>
+                <form
+                    className={css.searchForm}
+                    onSubmit={this.handleFormSubmit}
+                >
+                    <button type="submit" className={css.searchFormButton}>
+                        <span className={css.searchFormButtonLabel}>Search</span>
+                    </button>
+
+                    <input
+                        className={css.searchFormInput}
+                        type="text"
+                        autoComplete="off"
+                        autoFocus
+                        placeholder="Search images and photos"
+                        value={this.state.value}
+                        onChange={this.hadleInputChange}
+                    />
+                </form>
+            </header>
+        );
+    }; 
 };
